c07ae48a319e990791b2dcea51b31868b1f04c46
TantoluwaHeritageA <heritage1609@gmail.com>
2022-04-06 21:05:12 +0300

    fizzbuzz

---
+++ script.js
@@ -0,0 +1,9 @@
+function divisible(val){
+    for(let i = 1; i < val; i ++){
+     if(i % 3 === 0 && i % 5 === 0) {console.log( "FizzBuzz")}
+     else if(i % 3 === 0) {console.log( "Fizz")}
+     else if(i % 5 === 0) {console.log( "Buzz")}
+      else {console.log(i)}
+  }
+ }
+ divisible(16)
